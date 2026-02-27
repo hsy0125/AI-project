29bca66724d69ae412af9a3895c2102002c3b9a9
user <ocsy0125@naver.com>
2026-02-27 14:39:36 +0900

    Initial commit - WindowsForms Paint assignment

---
+++ WindowsFormsAppPaint_2_25/WindowsFormsAppPaint_2_25/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace WindowsFormsAppPaint_2_25
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.cbLineThickness = new System.Windows.Forms.ComboBox();
+			this.cbLineColor = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnDrawLine = new System.Windows.Forms.Button();
+			this.btnDrawRect = new System.Windows.Forms.Button();
+			this.btnDrawEllipse = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.열기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.열기ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.저장ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.btnUndo = new System.Windows.Forms.Button();
+			this.btnRedo = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::WindowsFormsAppPaint_2_25.Properties.Resources.cat;
+			this.pictureBox1.Location = new System.Drawing.Point(31, 69);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(500, 400);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+			this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+			// 
+			// cbLineThickness
+			// 
+			this.cbLineThickness.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbLineThickness.FormattingEnabled = true;
+			this.cbLineThickness.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+			this.cbLineThickness.Location = new System.Drawing.Point(622, 69);
+			this.cbLineThickness.Name = "cbLineThickness";
+			this.cbLineThickness.Size = new System.Drawing.Size(166, 26);
+			this.cbLineThickness.TabIndex = 1;
+			this.cbLineThickness.SelectedIndexChanged += new System.EventHandler(this.cbLineThickness_SelectedIndexChanged);
+			// 
+			// cbLineColor
+			// 
+			this.cbLineColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbLineColor.FormattingEnabled = true;
+			this.cbLineColor.Items.AddRange(new object[] {
+            "검정색",
+            "빨간색",
+            "주황색",
+            "노란색",
+            "초록색",
+            "파란색",
+            "보라색",
+            "흰색"});
+			this.cbLineColor.Location = new System.Drawing.Point(622, 113);
+			this.cbLineColor.Name = "cbLineColor";
+			this.cbLineColor.Size = new System.Drawing.Size(166, 26);
+			this.cbLineColor.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(538, 76);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(68, 18);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "선 두께";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(541, 129);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(68, 18);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "선 색상";
+			// 
+			// btnDrawLine
+			// 
+			this.btnDrawLine.Location = new System.Drawing.Point(544, 197);
+			this.btnDrawLine.Name = "btnDrawLine";
+			this.btnDrawLine.Size = new System.Drawing.Size(254, 73);
+			this.btnDrawLine.TabIndex = 5;
+			this.btnDrawLine.Text = "선 그리기";
+			this.btnDrawLine.UseVisualStyleBackColor = true;
+			this.btnDrawLine.Click += new System.EventHandler(this.btnDrawLine_Click);
+			// 
+			// btnDrawRect
+			// 
+			this.btnDrawRect.Location = new System.Drawing.Point(544, 276);
+			this.btnDrawRect.Name = "btnDrawRect";
+			this.btnDrawRect.Size = new System.Drawing.Size(254, 73);
+			this.btnDrawRect.TabIndex = 6;
+			this.btnDrawRect.Text = "사각형 그리기";
+			this.btnDrawRect.UseVisualStyleBackColor = true;
+			this.btnDrawRect.Click += new System.EventHandler(this.btnDrawRect_Click);
+			// 
+			// btnDrawEllipse
+			// 
+			this.btnDrawEllipse.Location = new System.Drawing.Point(544, 355);
+			this.btnDrawEllipse.Name = "btnDrawEllipse";
+			this.btnDrawEllipse.Size = new System.Drawing.Size(254, 73);
+			this.btnDrawEllipse.TabIndex = 7;
+			this.btnDrawEllipse.Text = "원형 그리기";
+			this.btnDrawEllipse.UseVisualStyleBackColor = true;
+			this.btnDrawEllipse.Click += new System.EventHandler(this.btnDrawEllipse_Click);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1030, 33);
+			this.menuStrip1.TabIndex = 8;
+			this.menuStrip1.Text = "파일 열기 저장";
+			// 
+			// 파일ToolStripMenuItem
+			// 
+			this.파일ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.열기ToolStripMenuItem,
+            this.열기ToolStripMenuItem1,
+            this.저장ToolStripMenuItem});
+			this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+			this.파일ToolStripMenuItem.Size = new System.Drawing.Size(64, 29);
+			this.파일ToolStripMenuItem.Text = "파일";
+			// 
+			// 열기ToolStripMenuItem
+			// 
+			this.열기ToolStripMenuItem.Name = "열기ToolStripMenuItem";
+			this.열기ToolStripMenuItem.Size = new System.Drawing.Size(210, 34);
+			this.열기ToolStripMenuItem.Text = "새로 만들기";
+			this.열기ToolStripMenuItem.Click += new System.EventHandler(this.열기ToolStripMenuItem_Click);
+			// 
+			// 열기ToolStripMenuItem1
+			// 
+			this.열기ToolStripMenuItem1.Name = "열기ToolStripMenuItem1";
+			this.열기ToolStripMenuItem1.Size = new System.Drawing.Size(210, 34);
+			this.열기ToolStripMenuItem1.Text = "열기";
+			this.열기ToolStripMenuItem1.Click += new System.EventHandler(this.열기ToolStripMenuItem1_Click);
+			// 
+			// 저장ToolStripMenuItem
+			// 
+			this.저장ToolStripMenuItem.Name = "저장ToolStripMenuItem";
+			this.저장ToolStripMenuItem.Size = new System.Drawing.Size(210, 34);
+			this.저장ToolStripMenuItem.Text = "저장";
+			this.저장ToolStripMenuItem.Click += new System.EventHandler(this.저장ToolStripMenuItem_Click);
+			// 
+			// btnUndo
+			// 
+			this.btnUndo.Enabled = false;
+			this.btnUndo.Location = new System.Drawing.Point(853, 69);
+			this.btnUndo.Name = "btnUndo";
+			this.btnUndo.Size = new System.Drawing.Size(101, 39);
+			this.btnUndo.TabIndex = 9;
+			this.btnUndo.Text = "실행취소";
+			this.btnUndo.UseVisualStyleBackColor = true;
+			this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+			// 
+			// btnRedo
+			// 
+			this.btnRedo.Enabled = false;
+			this.btnRedo.Location = new System.Drawing.Point(853, 114);
+			this.btnRedo.Name = "btnRedo";
+			this.btnRedo.Size = new System.Drawing.Size(101, 33);
+			this.btnRedo.TabIndex = 10;
+			this.btnRedo.Text = "되돌리기";
+			this.btnRedo.UseVisualStyleBackColor = true;
+			this.btnRedo.Click += new System.EventHandler(this.btnRedo_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1030, 690);
+			this.Controls.Add(this.btnRedo);
+			this.Controls.Add(this.btnUndo);
+			this.Controls.Add(this.btnDrawEllipse);
+			this.Controls.Add(this.btnDrawRect);
+			this.Controls.Add(this.btnDrawLine);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.cbLineColor);
+			this.Controls.Add(this.cbLineThickness);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.KeyPreview = true;
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ComboBox cbLineThickness;
+		private System.Windows.Forms.ComboBox cbLineColor;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnDrawLine;
+		private System.Windows.Forms.Button btnDrawRect;
+		private System.Windows.Forms.Button btnDrawEllipse;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 열기ToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem 저장ToolStripMenuItem;
+		private System.Windows.Forms.Button btnUndo;
+		private System.Windows.Forms.Button btnRedo;
+	}
+}
+
---
+++ WindowsFormsAppPaint_2_25/WindowsFormsAppPaint_2_25/Form1.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+/*
+ * 그림판 만들기 (기능 정리)
+ * (1) 파일 - (새로 만들기, 열기, 저장)
+ * (2) 선 두께 / 색상 선택
+ * (3) 선 그리기 / 사각형 그리기 / 원형 그리기
+ * (4) 실행취소 / 되돌리기 (단축키: Ctrl+N / Ctrl+O / Ctrl+S / Ctrl+Z / Ctrl+Y)
+ */
+
+namespace WindowsFormsAppPaint_2_25
+{
+	public partial class Form1 : Form
+	{
+		// ===============================
+		// (0) Enum 정의
+		// ===============================
+		enum DrawColor : int
+		{
+			ColorBlack = 0,
+			ColorRed,
+			ColorOrange,
+			ColorYellow,
+			ColorGreen,
+			ColorBlue,
+			ColorPurple,
+			ColorWhite
+		}
+
+		enum DrawType
+		{
+			DrawNone = 0,
+			DrawLine,
+			DrawRectangle,
+			DrawCircle
+		}
+
+		// ===============================
+		// (1) 필드 변수 영역 (State)
+		// ===============================
+		// 시작 점
+		private Point _startPos;
+
+		// 현재 위치
+		private Point _currentPos;
+
+		// 드래그 여부
+		private bool _isDrawing = false;
+
+		// 선 두께
+		private int _lineThickness = 2;
+
+		// 도형 타입
+		private DrawType _drawType = DrawType.DrawNone;
+
+		// 캔버스(누적 그림) + Undo/Redo 스택
+		private Bitmap _canvas;
+		private Stack<Bitmap> _undoStack = new Stack<Bitmap>();
+		private Stack<Bitmap> _redoStack = new Stack<Bitmap>();
+
+		// ===============================
+		// (2) 생성자 / 초기화
+		// ===============================
+		public Form1()
+		{
+			InitializeComponent();
+			InitCanvas(); // 시작 시 캔버스 생성
+			
+			this.KeyPreview = true;
+			this.KeyDown += Form1_KeyDown;
+		}
+
+		// 캔버스 초기화(새로 만들기)
+		private void InitCanvas()
+		{
+			_canvas?.Dispose();
+			_canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+			using (Graphics g = Graphics.FromImage(_canvas))
+			{
+				g.Clear(Color.White);
+			}
+
+			pictureBox1.Image = _canvas;
+
+			_undoStack.Clear();
+			_redoStack.Clear();
+
+			UpdateUndoRedoButtons();
+			pictureBox1.Invalidate();
+		}
+
+		// ===============================
+		// (3) 파일 메뉴 기능 (새로 만들기 / 열기 / 저장)
+		// ===============================
+		private void 열기ToolStripMenuItem_Click(object sender, EventArgs e) // (실제로는 새로 만들기 역할)
+		{
+			InitCanvas();
+		}
+
+		private void 열기ToolStripMenuItem1_Click(object sender, EventArgs e) // 파일 열기
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				using (var loaded = new Bitmap(openFileDialog1.FileName))
+				{
+					_canvas?.Dispose();
+					_canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+					using (Graphics g = Graphics.FromImage(_canvas))
+					{
+						g.Clear(Color.White);
+						// 캔버스 크기에 맞춰 이미지 그려넣기
+						g.DrawImage(loaded, 0, 0, pictureBox1.Width, pictureBox1.Height);
+					}
+
+					pictureBox1.Image = _canvas;
+				}
+
+				_undoStack.Clear();
+				_redoStack.Clear();
+				UpdateUndoRedoButtons();
+				pictureBox1.Invalidate();
+			}
+		}
+
+		private void 저장ToolStripMenuItem_Click(object sender, EventArgs e) // 저장
+		{
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				_canvas?.Save(saveFileDialog1.FileName);
+			}
+		}
+
+		// ===============================
+		// (4) UI 입력(ComboBox / Button) 이벤트
+		// ===============================
+		private void cbLineThickness_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			_lineThickness = Convert.ToInt32(cbLineThickness.SelectedItem);
+		}
+
+		private void btnDrawLine_Click(object sender, EventArgs e)
+		{
+			_drawType = DrawType.DrawLine;
+		}
+
+		private void btnDrawRect_Click(object sender, EventArgs e)
+		{
+			_drawType = DrawType.DrawRectangle;
+		}
+
+		private void btnDrawEllipse_Click(object sender, EventArgs e)
+		{
+			_drawType = DrawType.DrawCircle;
+		}
+
+		private void btnUndo_Click(object sender, EventArgs e)
+		{
+			Undo();
+		}
+
+		private void btnRedo_Click(object sender, EventArgs e)
+		{
+			Redo();
+		}
+
+		// ===============================
+		// (5) 마우스 드로잉 (Down / Move / Up) + Paint(미리보기)
+		// ===============================
+		private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button != MouseButtons.Left) return;
+			if (_drawType == DrawType.DrawNone) return;
+
+			_startPos = e.Location;
+			_currentPos = e.Location;
+			_isDrawing = true;
+		}
+
+		private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (!_isDrawing) return;
+
+			_currentPos = e.Location;
+			pictureBox1.Invalidate(); // Paint 다시 호출(미리보기)
+		}
+
+		private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+		{
+			if (e.Button != MouseButtons.Left) return;
+			if (!_isDrawing) return;
+
+			_isDrawing = false;
+			if (_canvas == null) return;
+
+			// 그리기 전 상태를 undo로 저장
+			PushUndoSnapshot();
+
+			// 캔버스에 실제로 그리기(누적 저장)
+			using (Graphics g = Graphics.FromImage(_canvas))
+			using (Pen pen = new Pen(GetSelColor(), _lineThickness))
+			{
+				switch (_drawType)
+				{
+					case DrawType.DrawLine:
+						g.DrawLine(pen, _startPos, _currentPos);
+						break;
+
+					case DrawType.DrawRectangle:
+						var rect = GetRectangle(_startPos, _currentPos);
+						g.DrawRectangle(pen, rect);
+						break;
+
+					case DrawType.DrawCircle:
+						var ellipse = GetRectangle(_startPos, _currentPos);
+						g.DrawEllipse(pen, ellipse);
+						break;
+				}
+			}
+
+			pictureBox1.Invalidate();
+		}
+
+		// PictureBox 그리기(드래그 중 미리보기만)
+		private void pictureBox1_Paint(object sender, PaintEventArgs e)
+		{
+			if (!_isDrawing) return;
+
+			using (Pen pen = new Pen(GetSelColor(), _lineThickness))
+			{
+				switch (_drawType)
+				{
+					case DrawType.DrawLine:
+						e.Graphics.DrawLine(pen, _startPos, _currentPos);
+						break;
+
+					case DrawType.DrawRectangle:
+						var rect = GetRectangle(_startPos, _currentPos);
+						e.Graphics.DrawRectangle(pen, rect);
+						break;
+
+					case DrawType.DrawCircle:
+						var ellipse = GetRectangle(_startPos, _currentPos);
+						e.Graphics.DrawEllipse(pen, ellipse);
+						break;
+				}
+			}
+		}
+
+		// ===============================
+		// (6) Undo / Redo 로직
+		// ===============================
+
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Control && e.KeyCode == Keys.N)
+			{
+				// Ctrl + N → 새로 만들기
+				InitCanvas();
+				e.SuppressKeyPress = true;
+			}
+			else if (e.Control && e.KeyCode == Keys.O)
+			{
+				// Ctrl + O → 열기
+				열기ToolStripMenuItem1_Click(null, null);
+				e.SuppressKeyPress = true;
+			}
+			else if (e.Control && e.KeyCode == Keys.S)
+			{
+				// Ctrl + S → 저장
+				저장ToolStripMenuItem_Click(null, null);
+				e.SuppressKeyPress = true;
+			}
+			else if (e.Control && e.KeyCode == Keys.Z)
+			{
+				// Ctrl + Z → 실행취소
+				Undo();
+				e.SuppressKeyPress = true;
+			}
+			else if (e.Control && e.KeyCode == Keys.Y)
+			{
+				// Ctrl + Y → 다시 실행
+				Redo();
+				e.SuppressKeyPress = true;
+			}
+		}
+		private void PushUndoSnapshot()
+		{
+			if (_canvas == null) return;
+
+			_undoStack.Push((Bitmap)_canvas.Clone()); // 현재 상태 저장
+			_redoStack.Clear();                       // 새 작업 시작하면 redo 초기화
+			UpdateUndoRedoButtons();
+		}
+
+		private void Undo()
+		{
+			if (_undoStack.Count == 0 || _canvas == null) return;
+
+			_redoStack.Push((Bitmap)_canvas.Clone());
+
+			_canvas.Dispose();
+			_canvas = _undoStack.Pop();
+
+			pictureBox1.Image = _canvas;
+			pictureBox1.Invalidate();
+
+			UpdateUndoRedoButtons();
+		}
+
+		private void Redo()
+		{
+			if (_redoStack.Count == 0 || _canvas == null) return;
+
+			_undoStack.Push((Bitmap)_canvas.Clone());
+
+			_canvas.Dispose();
+			_canvas = _redoStack.Pop();
+
+			pictureBox1.Image = _canvas;
+			pictureBox1.Invalidate();
+
+			UpdateUndoRedoButtons();
+		}
+
+		private void UpdateUndoRedoButtons()
+		{
+			btnUndo.Enabled = _undoStack.Count > 0;
+			btnRedo.Enabled = _redoStack.Count > 0;
+		}
+
+		// ===============================
+		// (7) 유틸리티(색상/사각형 계산)
+		// ===============================
+		private Color GetSelColor()
+		{
+			Color clrLine = new Color();
+
+			DrawColor drawColor = (DrawColor)cbLineColor.SelectedIndex;
+
+			switch (drawColor)
+			{
+				case DrawColor.ColorBlack:
+					clrLine = Color.Black; break;
+				case DrawColor.ColorRed:
+					clrLine = Color.Red; break;
+				case DrawColor.ColorOrange:
+					clrLine = Color.Orange; break;
+				case DrawColor.ColorYellow:
+					clrLine = Color.Yellow; break;
+				case DrawColor.ColorGreen:
+					clrLine = Color.Green; break;
+				case DrawColor.ColorBlue:
+					clrLine = Color.Blue; break;
+				case DrawColor.ColorPurple:
+					clrLine = Color.Purple; break;
+				case DrawColor.ColorWhite:
+					clrLine = Color.White; break;
+				default:
+					clrLine = Color.Black; break;
+			}
+
+			return clrLine;
+		}
+
+		private Rectangle GetRectangle(Point p1, Point p2)
+		{
+			return new Rectangle(
+				Math.Min(p1.X, p2.X),
+				Math.Min(p1.Y, p2.Y),
+				Math.Abs(p2.X - p1.X),
+				Math.Abs(p2.Y - p1.Y));
+		}
+	}
+}
